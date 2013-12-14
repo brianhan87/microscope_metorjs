f9aadb12fee01389bf78e7017599e8bf0bdc1b3a
Brian Han <brianhan87usa@gmail.com>
2013-12-13 23:55:31 -0500

    Use a method to submit the post

--- collections/posts.js
+++ collections/posts.js
@@ -1,14 +1,34 @@
 Posts = new Meteor.Collection('posts');
 
-// To Var Or Not To Var?
-// In Meteor, 
-// the var keyword limits the scope of an object to the current file. 
-// We want to make the Posts collection available to our whole app, 
-// which is why we're omitting that keyword here.
+Meteor.methods({
+  post: function(postAttributes) {
+    var user = Meteor.user(),
+      postWithSameLink = Posts.findOne({url: postAttributes.url});
 
-Posts.allow({
-  insert: function(userId, doc) {
-    // only allow positing if you are logged in 
-    return !! userId;
+    // ensure the user is logged in
+    if (!user)
+      throw new Meteor.Error(401, "You need to login to post new stories");
+
+    // ensure the post has a title
+    if (!postAttributes.title)
+      throw new Meteor.Error(422, 'Please fill in a headline');
+
+    // check that there are no previous posts with the same link
+    if (postAttributes.url && postWithSameLink) {
+      throw new Meteor.Error(302, 
+        'This link has already been posted', 
+        postWithSameLink._id);
+    }
+
+    // pick out the whitelisted keys
+    var post = _.extend(_.pick(postAttributes, 'url', 'title', 'message'), {
+      userId: user._id, 
+      author: user.username, 
+      submitted: new Date().getTime()
+    });
+
+    var postId = Posts.insert(post);
+
+    return postId;
   }
 });
