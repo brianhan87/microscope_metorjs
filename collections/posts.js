a1589527f9d5952cc770d5354506f4c7d172ab04
Brian Han <brianhan87usa@gmail.com>
2013-12-13 21:20:13 -0500

    Add comment about declaring global scope by omitting var

--- collections/posts.js
+++ collections/posts.js
@@ -1,1 +1,7 @@
 Posts = new Meteor.Collection('posts');
+
+// To Var Or Not To Var?
+// In Meteor, 
+// the var keyword limits the scope of an object to the current file. 
+// We want to make the Posts collection available to our whole app, 
+// which is why we're omitting that keyword here.
