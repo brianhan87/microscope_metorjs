ed6bbbdaf132760266e53ef9becf8d7b834f13b6
Brian Han <brianhan87usa@gmail.com>
2013-12-13 23:28:25 -0500

    Logged in users have permission to write posts

--- collections/posts.js
+++ collections/posts.js
@@ -5,3 +5,10 @@ Posts = new Meteor.Collection('posts');
 // the var keyword limits the scope of an object to the current file. 
 // We want to make the Posts collection available to our whole app, 
 // which is why we're omitting that keyword here.
+
+Posts.allow({
+  insert: function(userId, doc) {
+    // only allow positing if you are logged in 
+    return !! userId;
+  }
+});
